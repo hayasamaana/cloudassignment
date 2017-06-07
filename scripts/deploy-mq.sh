70aaa9e6a6bc06ad2c175fe1a5551c64e491e2d2
Per Boström <per.bostrom@liu.se>
2017-06-07 13:48:58 +0200

    new deploy scripts

--- scripts/deploy-mq.sh
+++ scripts/deploy-mq.sh
@@ -1,2 +1,23 @@
 #!/bin/sh
 python vmanager.py -c ../src/service/waspmq.sh -a create waspmq
+
+
+while [ 1 ]
+do
+    b=$(python vmanager.py -a show-ip waspmq)
+    echo $b
+    if [ "$b"="" ]
+    then
+            echo "IP not found"
+    else
+            echo "IP found"
+            a="server="
+            c="$a$b"
+            awk -v var="$c" 'NR==3 {$0=var} 1' ../src/service/credentials.txt > ../src/service/credentials.txt
+            exit
+    fi
+    sleep 2
+done
+
+git commit -am "new message queue (automatic commit!!!!)"
+git push
