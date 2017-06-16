f97d555e6a7b9a29473f4cb92daec8b5f86a199a
Per Boström <per.bostrom@liu.se>
2017-06-16 10:50:47 +0200

    swiftclients-cred

---
+++ scripts/swiftclient-credentials.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#Environment variables for swiftclient
+export ST_AUTH_VERSION=3
+export OS_USERNAME=ercx026
+export OS_PASSWORD=ercx026
+export OS_PROJECT_NAME=LiU-2
+export OS_USER_DOMAIN_NAME=xerces
+export OS_PROJECT_DOMAIN_NAME=xerces
+export OS_AUTH_URL=https://xerces.ericsson.net:5000/v3
