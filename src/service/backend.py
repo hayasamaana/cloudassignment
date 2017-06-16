a79d2f3967589718c07babbf912611888d2aea14
Per Boström <per.bostrom@liu.se>
2017-06-16 12:43:07 +0200

    backend.py indentations

--- src/service/backend.py
+++ src/service/backend.py
@@ -40,9 +40,9 @@ def callback(ch, method, properties, body):
     # convert video
     success, convertedVideoName = convertVideo(msg["VideoName"])
     if not success:
-         print(" [x] Conversion failed")
-         ch.basic_ack(delivery_tag = method.delivery_tag)
-         return
+        print(" [x] Conversion failed")
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+        return
 
     # upload converted video
     success = uploadVideo("Videos/"+convertedVideoName, msg["convertedVideoName"])
@@ -55,37 +55,33 @@ def callback(ch, method, properties, body):
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
 def receive(connection_info=None):
-	qname = "wasp"
-	credentials = pika.PlainCredentials(
-        connection_info["username"], connection_info["password"])
-	connection = pika.BlockingConnection(pika.ConnectionParameters(
-        connection_info["server"],connection_info["port"],'/',credentials))
-	channel = connection.channel()
-
-	channel.queue_declare(queue=qname)
+    qname = "wasp"
+    credentials = pika.PlainCredentials(connection_info["username"], connection_info["password"])
+    connection = pika.BlockingConnection(pika.ConnectionParameters(connection_info["server"],connection_info["port"],'/',credentials))
+    channel = connection.channel()
+    channel.queue_declare(queue=qname)
     channel.basic_qos(prefetch_count=1)
-	channel.basic_consume(callback, queue=qname, no_ack=True)
-	print(' [*] Waiting for messages. To exit press CTRL+C')
-	channel.start_consuming()
+    channel.basic_consume(callback, queue=qname, no_ack=True)
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
 
 
 if __name__=="__main__":
-	parser = OptionParser()
+    parser = OptionParser()
     parser.add_option('-c', '--credential', dest='credentialFile',default="../../etc/credentials/mq-credentials.txt", help='Path to CREDENTIAL file', metavar='CREDENTIALFILE')
-	(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
     subprocess.call("../../scripts/./swiftclient-credentials.sh", shell=True)
 
     if options.credentialFile:
-		config = ConfigParser.RawConfigParser()
-		config.read(options.credentialFile)
-		connection = {}
-		connection["server"] = config.get('rabbit', 'server')
-		connection["port"] = int(config.get('rabbit', 'port'))
-		connection["queue"] = config.get('rabbit', 'queue')
-		connection["username"] = config.get('rabbit', 'username')
-		connection["password"] = config.get('rabbit', 'password')
-		receive(connection_info=connection)
-	else:
-		print("Syntax: 'python backend.py -h' | '--help' for help")
-        
+        config = ConfigParser.RawConfigParser()
+        config.read(options.credentialFile)
+        connection = {}
+        connection["server"] = config.get('rabbit', 'server')
+        connection["port"] = int(config.get('rabbit', 'port'))
+        connection["queue"] = config.get('rabbit', 'queue')
+        connection["username"] = config.get('rabbit', 'username')
+        connection["password"] = config.get('rabbit', 'password')
+        receive(connection_info=connection)
+    else:
+        print("Syntax: 'python backend.py -h' | '--help' for help")
